79737de5b3f7b153f34075df9c097e527b9dcffb
Yi Wang <yi.wang.2005@gmail.com>
2016-07-27 20:32:04 -0700

    Minor fixes

--- bash_ops_test.go
+++ bash_ops_test.go
@@ -20,23 +20,25 @@ func TestHead(t *testing.T) {
 	assert.Equal(t, "Hello", <-out)
 	assert.Equal(t, "World!", <-out)
 
+	assert.Equal(t, 0, Wc(Head(Echo("Hello\nWorld!"), -1)))
+	assert.Equal(t, 0, Wc(Head(Echo("Hello\nWorld!"), 0)))
 	assert.Equal(t, 1, Wc(Head(Echo("Hello\nWorld!"), 1)))
 	assert.Equal(t, 2, Wc(Head(Echo("Hello\nWorld!"), 2)))
 	assert.Equal(t, 2, Wc(Head(Echo("Hello\nWorld!"), 3)))
 }
 
-func TestEach_ToFile_Cat_Du_Grep_For(t *testing.T) {
+func TestEcho_ToFile_Cat_Du_Grep_For(t *testing.T) {
 	dir, e := ioutil.TempDir("", "")
 	candy.Must(e)
 
-	filename := path.Join(dir, "TestToFile")
+	filename := path.Join(dir, "sh_test")
 	ToFile(Echo("Hello\nWorld!"), filename)
 
 	out := Cat(filename)
 	assert.Equal(t, "Hello", <-out)
 	assert.Equal(t, "World!", <-out)
 
-	assert.Equal(t, filename, <-Grep(Du(dir), "TestToFile"))
+	assert.Equal(t, filename, <-Grep(Du(dir), "sh_test"))
 
 	assert.Equal(t, "Hello",
 		<-For(Du(dir), func(filename string) chan string {
